f9fa415fbd93e963eb4bc38f65a2af63ea62e05d
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-11-09 13:31:21 -0800

    Unify GOPATH as gocode on different laptops

---
+++ .history/controller/home_20191108232021.go
@@ -0,0 +1,60 @@
+package controller
+
+import (
+	"github.com/musketeer-liu/Go_Mega_Project/vm"
+	"net/http"
+)
+
+type home struct {}
+
+func (h home) registerRoutes() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpName := "index.html"
+	vop := vm.IndexViewModelOp{}
+	v := vop.GetVM()
+	templates[tpName].Execute(w, &v)
+}
+
+func check(username, password string) bool {
+	if username == "musketeer-liu" && password == "c5d19806cc93103487e7b3c7b2e2342c48e931c9" {
+		return true
+	}
+	return false
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	tpName := "login.html"
+	vop := vm.LoginViewModelOp{}
+	v := vop.GetVM()
+
+	if r.Method == http.MethodGet {
+		templates[tpName].Execute(w, &w)
+	}
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+
+		if len(username) < 3 {
+			v.AddError("username must longer than 3")
+		}
+
+		if len(password) < 6 {
+			v.AddError("password must longer than 6")
+		}
+
+		if !check(username, password) {
+			v.AddError("username password not correct, please input again")
+		}
+
+		if len(v.Errs) > 0 {
+			templates[tpName].Execute(w, &v)
+		} else {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		}
+	}
+}

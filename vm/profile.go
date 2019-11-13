a6a52eb895b10a3d119fac377ccd678e9e759035
Yutong Liu <yutong.liu@uber.com>
2019-11-13 11:29:01 -0800

    Add Follower in controller and view

--- vm/profile.go
+++ vm/profile.go
@@ -9,6 +9,9 @@ type ProfileViewModel struct {
 	BaseViewModel
 	Posts			[]model.Post
 	Editable		bool
+	IsFollow		bool
+	FollowersCount	int
+	FollowingCount	int
 	ProfileUser		model.User
 }
 
@@ -19,14 +22,37 @@ type ProfileViewModelOp struct {}
 func (ProfileViewModelOp) GetVM(sUser, pUser string) (ProfileViewModel, error) {
 	v := ProfileViewModel{}
 	v.SetTitle("Profile")
-	u1, err := model.GetUserByUsername(pUser)
+	u, err := model.GetUserByUsername(pUser)
 	if err != nil {
 		return v, err
 	}
-	posts, _ := model.GetPostsByUserID(u1.ID)
-	v.ProfileUser = *u1
+	posts, _ := model.GetPostsByUserID(u.ID)
+	v.ProfileUser = *u
 	v.Editable = (sUser == pUser)
+	if !v.Editable {
+		v.IsFollow = u.IsFollowedByUser(sUser)
+	}
+	v.FollowersCount = u.FollowersCount()
+	v.FollowingCount = u.FollowingCount()
 	v.Posts = *posts
 	v.SetCurrentUser(sUser)
 	return v, nil
+}
+
+// Follow func: A follow B
+func Follow(a, b string) error {
+	u, err := model.GetUserByUsername(a)
+	if err != nil {
+		return err
+	}
+	return u.Follow(b)
+}
+
+// UnFollow func: A unfollow B
+func UnFollow(a, b string) error {
+	u, err := model.GetUserByUsername(a)
+	if err != nil {
+		return err
+	}
+	return u.Unfollow(b)
 }
